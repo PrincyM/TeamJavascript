16ccb745deb3ebc11cb4f1c6e286dbf593c8d437
diego <dfmoncada@gmail.com>
2017-12-09 17:31:45 -0500

    added new commented code to embed external websites.

---
+++ javascript/site-map.js
@@ -0,0 +1,11 @@
+//alert("hola");
+// ###########################################
+// ######## CODE FOR EXTERNAL WEB ############
+// ###########################################
+// function onLoadProcess(){
+// 	alert("hola");
+// 	$("#siteloader").html('<object id="new-page" data="http://emol.com/"></object>');
+// 	$("#new-page").css("width","1080px")
+// 	$("#new-page").css("height","1600");
+// }
+// window.onload = onLoadProcess;
