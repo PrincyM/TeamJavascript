f3d180e6a39006aefcaff351bf35ae7ff2978a21
Princy Mascarenhas <princymascarenhas@Princys-MacBook-Pro.local>
2018-01-11 15:04:15 -0500

    adding menu images in a separate folder under images folder

---
+++ javascript/menu.js
@@ -0,0 +1,19 @@
+
+// alert("connected");
+jQuery(document).ready(function () {
+
+// $('p').hide();
+
+// $('img').click(function() {
+//  $('.contentBox').hide();
+// $(this).next('p').slideToggle(3000);
+//
+// })
+//
+// $('p').hover(
+// function() { $('p').css({'background': '#FFE5C3', 'color': '#524737'});}, // multiple css elements should include curly brackets
+// function() { $('p').css({'background': '#524737', 'color': '#FFE5C3'});}
+//
+// );
+
+});
