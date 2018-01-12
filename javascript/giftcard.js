5ccf9654942b3bcc4e46d33a2bc5a73193caa727
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-01-11 20:16:20 -0500

    as of Jan 11th

--- javascript/giftcard.js
+++ javascript/giftcard.js
@@ -1,7 +1,48 @@
-jQuery(document).ready(function(){
-	$("#copper_star").remove();
-	$("#green_star").remove();
-	$("#red_star").remove();
+// stars hidden
+
+$(document).ready(function(){  
+
+//hide the stars
+
+	$("#copper_star").hide();
+	$("#green_star").hide();     
+	$("#red_star").hide();
+
+//functions to take user input
+
+	$("#fname").on('input',onFnameChange);
+	$("#to_giftee").on('input',onToNameChange);
+	
+
+
+
+// functions change background color
+
+	$("#btn_bg_grey").click(function(){$("#giftcard").css
+	("background-color", "#bcbaba"); } ); 
+
+	$("#btn_bg_green").click(function(){$("#giftcard").css
+	("background-color", "#cccc98"); } );
+
+	$("#btn_bg_red").click(function(){$("#giftcard").css
+	("background-color", "#ac2430"); } );
+
+//  functions to change star 
+
+	$("#star_copper").click(function(){
+		$("#original_logo").hide();
+		$("#copper_star").show();
+	});
 
 
 })//end of ready of line 1
+
+function onFnameChange(){
+	var a = $("#fname").val();
+	$("#from_name").html(a);
+}
+
+function onToNameChange(){
+	var a = $("#to_giftee").val();
+	$("#to_name").html(a);
+}
