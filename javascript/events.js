5400a8c5a61189b037fc4fea83600a41412d7729
matthewjweber@gmail.com <matthewjweber@gmail.com>
2018-01-12 17:39:06 -0500

    here we gooo

---
+++ javascript/events.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+document.getElementById("comradeform").addEventListener('submit', submitForm);
+
+
+function submitForm() {
+	
+		event.preventDefault();
+        var formName = document.getElementById('name').value;
+        var formEmail = document.getElementById('email').value;
+        var formDate = document.getElementById('date').value;
+		var formMessage = document.getElementById('message').value;
+			
+			
+	
+		document.getElementById("formwrapper").style.display = "none";
+
+		
+
+        document.getElementById('displayform').style.display = "block";
+	
+		document.getElementById('displayname').innerHTML = formName;
+		document.getElementById('displayemail').innerHTML = formEmail;
+		document.getElementById('displaydate').innerHTML = formDate;
+		document.getElementById('displaymessage').innerHTML = formMessage;
+        return false;
+
+    }
+
+
+}
