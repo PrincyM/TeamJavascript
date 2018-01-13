664c38f0b60fed7163516dcfcc11c6351ef73d7f
Princy Mascarenhas <princymascarenhas@Princys-MacBook-Pro.local>
2018-01-13 17:12:16 -0500

    changes to menu text shadow

--- javascript/menu.js
+++ javascript/menu.js
@@ -12,11 +12,7 @@ $('.gallery-item').hover(
   function() {
     $(this).find('.menu_img').css('opacity','1');
         $(this).find('.img-title').fadeOut(100);
-      },
-
-  // function() {
-  //      $(this).find('.menu_img').css('opacity','0.3');
-  //    }
+      }
 
     );
 
