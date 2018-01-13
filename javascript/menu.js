e5d8f34595ab2c82b89bf39febe537dcf6f6813e
Princy Mascarenhas <princymascarenhas@Princys-MacBook-Pro.local>
2018-01-13 16:47:37 -0500

    updated menu page

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,31 +1,23 @@
 
-// alert("connected");
-// jQuery(document).ready(function () {
-//
-// $('.menu-details').hide();
-// // $('.menu-heading').hide();
-//
-// $('.menu-heading').click(function() {
-//   $(this).next('.menu-details').slideToggle(1000);
-// })
-// });
 
-jQuery(document).ready(function () {
-
-// $('.menu-details').hide();
-// $('.menu-heading').hide();
-
-$('.menu-heading').click(function() {
-  $(this).next('.menu-details').slideToggle(1000);
-});
+$(document).ready(function () {
 
 
-
-$('.gallery-item').hover( function() {
+$('.gallery-item').hover(
+  function() {
+      $(this).find('.menu_img').css('opacity','0.4');
+      $(this).find('.img-title').css('opacity','1');
         $(this).find('.img-title').fadeIn(300);
-    }, function() {
+    },
+  function() {
+    $(this).find('.menu_img').css('opacity','1');
         $(this).find('.img-title').fadeOut(100);
+      },
+
+  // function() {
+  //      $(this).find('.menu_img').css('opacity','0.3');
+  //    }
+
+    );
+
     });
-
-
-});
