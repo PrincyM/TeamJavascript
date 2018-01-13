5b5421884f37e2e27f84dbfb73a29538a23bbb9e
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-01-13 10:47:59 -0500

    about page js file added

---
+++ javascript/about.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){  
+
+//INITAL POSITION
+
+	$("#div_ms").hide();
+
+//BUTTONS
+
+	$("#btn_ms").click(function() {
+	$("#div_ms").show(300);
+})
+
+}); // END OF document.ready
