3e6477743c9f32ebf2a9886f9acd3c39b30a5c44
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-01-12 22:44:11 -0500

    slow but steady progess with location page

--- javascript/location.js
+++ javascript/location.js
@@ -1,5 +1,5 @@
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var uluru = {lat: 43.744136, lng: -79.211772};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 14,
           center: uluru
